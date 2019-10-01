c7288d276fdaca57e7401c9f5148c0de0c48da71
jharte5 <jonathan.harte@codeimmersives.com>
2019-10-01 10:37:15 -0400

    submit

--- main.js
+++ main.js
@@ -1,60 +1,46 @@
-function moreThan5() {
-
-}
-
-function topScore() {
-  
-}
-
-function isInDanger() {
-
-}
-
-function isCoasting() {
-
-}
-
-function isSucceeding() {
-
-}
-
-function isFailing() {
-
-}
-
-function isAcing() {
-
-}
-
-function isStudent() {
-
-}
-
-function isTeacher() {
-
-}
-
-function isAdmin() {
-
-}
-
-function isElementary() {
-
-}
-
-function notAnElementarySchoolAdministrator() {
-
-}
-
-function isMiddleSchoolTeacher() {
-
-}
-
-function differentPeople() {
-
-}
-
-module.exports = {
+function moreThan5(num) {
+  return 5<num;
+ }
+ function topScore(score,topScore) {
+  return score>topScore;
+ }
+ function isInDanger(grade) {
+  return 60<=grade && grade<=71;
+ }
+ function isCoasting(num) {
+  return 71<num && 83>=num;
+ }
+ function isSucceeding(grade) {
+  return grade>=84 && grade<=92;
+ }
+ function isFailing(grade) {
+  return grade<60;
+ }
+ function isAcing(grade) {
+  return grade>92;
+ }
+ function isStudent(student) {
+  return student === 'student'
+ }
+ function isTeacher(teacher) {
+  return teacher === 'teacher'
+ }
+ function isAdmin(admin) {
+  return admin === 'admin'
+ }
+ function isElementary(schoolLevel) {
+  return schoolLevel === 'elementary'
+ }
+ function notAnElementarySchoolAdministrator(role, level) {
+  return role !== 'elementary' || level !== 'admin'
+ }
+ function isMiddleSchoolTeacher(role, level) {
+  return role === 'teacher' && level>=6 && level<=8;
+ }
+ function differentPeople(jon1, jon2) {
+  return jon1 === jon2;
+ }
+ module.exports = {
   moreThan5,
   topScore,
   isInDanger,
@@ -69,4 +55,13 @@ module.exports = {
   notAnElementarySchoolAdministrator,
   isMiddleSchoolTeacher,
   differentPeople,
-}
+ }
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
